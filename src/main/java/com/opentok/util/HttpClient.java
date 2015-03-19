19753fa4847583e38a6fc52b516a77be89014b80
R. Alex Matevish <alex@matevi.sh>
2015-03-19 12:48:49 -0700

    Updated HttpClient to use URLFetchServiceFactory correctly.

--- src/main/java/com/opentok/util/HttpClient.java
+++ src/main/java/com/opentok/util/HttpClient.java
@@ -12,8 +12,7 @@ import java.util.Map;
 import java.util.concurrent.Future;
 
 public class HttpClient implements URLFetchService {
-
-    private URLFetchService urlFetchService;
+    
     private final String apiUrl;
     private final int apiKey;
     private final String apiSecret;
@@ -22,7 +21,6 @@ public class HttpClient implements URLFetchService {
         this.apiKey = apiKey;
         this.apiUrl = apiUrl;
         this.apiSecret = apiSecret;
-        urlFetchService = URLFetchServiceFactory.getURLFetchService();
     }
 
     public String createSession(Map<String, Collection<String>> params) throws RequestException {
@@ -83,6 +81,7 @@ public class HttpClient implements URLFetchService {
         httpRequest.setHeader(new HTTPHeader("User-Agent", "Opentok-Java-SDK/" + Version.VERSION));
         httpRequest.setHeader(new HTTPHeader("X-TB-PARTNER-AUTH", this.apiKey + ":" + this.apiSecret));
 
+        URLFetchService urlFetchService = URLFetchServiceFactory.getURLFetchService();
         return urlFetchService.fetch(httpRequest);
     }
 
